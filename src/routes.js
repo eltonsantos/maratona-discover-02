366d5e038b99a83bdb49dba315e9fa3d04d4ae65
Elton Santos <eltonaxl@hotmail.com>
2021-03-25 23:02:26 -0300

    Final class 1

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+  name: "Elton Santos",
+  avatar: "https://avatars.githubusercontent.com/u/1292594?v=4",
+  "monthly-budget": "3000",
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4
+}
+
 routes.get('/', (req, res) => res.render(views + "index"));
 routes.get('/job', (req, res) => res.render(views + "job"));
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
-routes.get('/profile', (req, res) => res.render(views + "profile"));
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
 // routes.get('/index.html', (req, res) => {
 //   return res.redirect('/')
