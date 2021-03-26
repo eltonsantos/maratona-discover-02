b695d02b9fde3b382c9e2060ef21cc71c7d590f3
Elton Santos <eltonaxl@hotmail.com>
2021-03-25 22:40:02 -0300

    Other commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = express.Router();
+
+const views = __dirname + "/views/";
+
+routes.get('/', (req, res) => res.render(views + "index"));
+routes.get('/job', (req, res) => res.render(views + "job"));
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
+routes.get('/profile', (req, res) => res.render(views + "profile"));
+
+// routes.get('/index.html', (req, res) => {
+//   return res.redirect('/')
+// });
+
+module.exports = routes;
