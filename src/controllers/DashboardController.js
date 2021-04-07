a1c78783e4125e00cc554c122257ac6a2456f87f
Elton Santos <eltonaxl@hotmail.com>
2021-04-06 23:28:18 -0300

    Commiting with DB

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,30 +1,31 @@
-const Job = require("../model/Job");
-const JobUtils = require("../utils/JobUtils");
-const Profile = require("../model/Profile");
+const Job = require('../model/Job')
+const JobUtils = require('../utils/JobUtils')
+const Profile = require('../model/Profile')
 
 module.exports = {
-  index(req, res) {
-    const jobs = Job.get();
-    const profile = Profile.get();
+  async index(req, res) {
+    const jobs = await Job.get();
+    const profile = await Profile.get();
 
     let statusCount = {
-      progress: 0,
-      done: 0,
-      total: jobs.length,
-    };
+        progress: 0,
+        done: 0,
+        total: jobs.length
+    }
 
-    let jobTotalHours = 0;
+    // total de horas por dia de cada Job em progresso
+    let jobTotalHours = 0
 
     const updatedJobs = jobs.map((job) => {
+      // ajustes no job
       const remaining = JobUtils.remainingDays(job);
       const status = remaining <= 0 ? "done" : "progress";
-
+      
+      // Somando a quantidade de status
       statusCount[status] += 1;
-
-      jobTotalHours =
-        status == "progress"
-          ? (jobTotalHours += Number(job["daily-hours"]))
-          : jobTotalHours;
+    
+      // total de horas por dia de cada Job em progresso
+      jobTotalHours = status == 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
 
       return {
         ...job,
@@ -34,13 +35,11 @@ module.exports = {
       };
     });
 
+    // qtd de horas que quero trabalhar dia (PROFILE)
+    // MENOS 
+    // quatidade de horas/dia de cada job em progress
     const freeHours = profile["hours-per-day"] - jobTotalHours;
 
-    return res.render("index", {
-      jobs: updatedJobs,
-      profile: profile,
-      statusCount: statusCount,
-      freeHours: freeHours,
-    });
+    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours });
   },
 };
